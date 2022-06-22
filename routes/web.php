a7b641d8e8d7af63aeb442f95ed52e1c2200c7ea
Romanets Pavel <pavelroma24@gmail.com>
2022-06-22 07:44:33 +0300

    add: l_2_1

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin', 'name' => 'admin.'], function () {
-//    Route::get('/', [, 'index'])->name('index');
+//Route::group(['prefix' => 'admin', 'namespace' => '\App\Http\Controllers\Admin'], function () {
+//    Route::get('/', 'MainController@index')->name('admin.index');
+//});
+
+Route::prefix('admin')->namespace('\App\Http\Controllers\Admin')->name('admin.')->group(function () {
+    Route::get('/', 'MainController@index')->name('index');
 });
