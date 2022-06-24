4ca14bb7d0af9aa4bf7b74ced13385e0dbbbd120
Romanets Pavel <pavelroma24@gmail.com>
2022-06-24 09:36:30 +0300

    add: l_2_5

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,11 +2,31 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 // php artisan make:model Category -m
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
