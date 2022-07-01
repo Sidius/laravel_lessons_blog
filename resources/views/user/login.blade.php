787468adcc0612d6ddc38f14da882cbe9ac608a9
Romanets Pavel <pavelroma24@gmail.com>
2022-07-01 09:20:29 +0300

    add: l_2_13 (Login)

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Registration Page</title>
+    <title>AdminLTE 3 | Login Page</title>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -17,7 +17,7 @@
 <body class="hold-transition register-page">
 <div class="register-box">
     <div class="register-logo">
-        <b>Registration</b>
+        <b>Login</b>
     </div>
 
     <div class="card">
@@ -25,16 +25,14 @@
 
             @include('admin.layouts.message')
 
-            <form action="{{ route('register.store') }}" method="post">
-                @csrf
-                <div class="input-group mb-3">
-                    <input type="text" name="name" class="form-control" placeholder="Name" value="{{ old('name') }}">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-user"></span>
-                        </div>
-                    </div>
+            @if (session()->has('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
                 </div>
+            @endif
+
+            <form action="{{ route('login') }}" method="post">
+                @csrf
                 <div class="input-group mb-3">
                     <input type="email" name="email" class="form-control" placeholder="Email"
                            value="{{ old('email') }}">
@@ -52,25 +50,16 @@
                         </div>
                     </div>
                 </div>
-                <div class="input-group mb-3">
-                    <input type="password" name="password_confirmation" class="form-control"
-                           placeholder="Retype password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                </div>
                 <div class="row">
                     <!-- /.col -->
                     <div class="col-4 offset-8">
-                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
                     </div>
                     <!-- /.col -->
                 </div>
             </form>
 
-            <a href="{{ route('login.create') }}" class="text-center">I already have a membership</a>
+            <a href="#" class="text-center">I already have a membership</a>
         </div>
         <!-- /.form-box -->
     </div><!-- /.card -->
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 //    Route::get('/', 'MainController@index')->name('admin.index');
 //});
 
-Route::prefix('admin')->namespace('\App\Http\Controllers\Admin')->name('admin.')->group(function () {
+Route::prefix('admin')->namespace('\App\Http\Controllers\Admin')->name('admin.')->middleware('admin')->group(function () {
     Route::get('/', 'MainController@index')->name('index');
     Route::resource('/categories', 'CategoryController');
     Route::resource('/tags', 'TagController');
@@ -31,5 +31,12 @@ Route::prefix('admin')->namespace('\App\Http\Controllers\Admin')->name('admin.')
 // php artisan route:list
 // php artisan route:list --path=admin/cat
 
-Route::get('/register', '\App\Http\Controllers\UserController@create')->name('register.create');
-Route::post('/register', '\App\Http\Controllers\UserController@store')->name('register.store');
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', '\App\Http\Controllers\UserController@create')->name('register.create');
+    Route::post('/register', '\App\Http\Controllers\UserController@store')->name('register.store');
+    Route::get('/login', '\App\Http\Controllers\UserController@loginForm')->name('login.create');
+    Route::post('/login', '\App\Http\Controllers\UserController@login')->name('login');
+});
+
+Route::get('/logout', '\App\Http\Controllers\UserController@logout')->name('logout')->middleware('auth');
+
