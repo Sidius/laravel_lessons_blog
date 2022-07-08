654846bee36407310579cf45305d36c00d3a6877
Romanets Pavel <pavelroma24@gmail.com>
2022-07-08 08:12:22 +0300

    add: l_2_19

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function show($slug)
+    {
+        $category = Category::query()->where('slug', $slug)->firstOrFail();
+        $posts = $category->posts()->orderBy('id', 'desc')->paginate(2);
+        return view('categories.show', compact('category', 'posts'));
+    }
+}
