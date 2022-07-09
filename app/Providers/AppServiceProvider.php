637f285690c06eb923b38742e51d0a9994049f59
Romanets Pavel <pavelroma24@gmail.com>
2022-07-09 07:47:48 +0300

    up: l_2_21

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('layouts.sidebar', function ($view) {
+
+            if (Cache::has('cats')) {
+                $cats = Cache::get('cats');
+            } else {
+                $cats = Category::query()->withCount('posts')->orderBy('posts_count', 'desc')->get();
+                Cache::put('cats', $cats, 300);
+            }
+
+            $view->with('popular_posts', Post::query()->orderBy('views', 'desc')->limit(3)->get());
+            $view->with('cats', $cats);
+        });
     }
 }
